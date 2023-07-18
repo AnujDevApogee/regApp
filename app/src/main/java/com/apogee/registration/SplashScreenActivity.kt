e69861ebea29460c3c3ab6425b9da745192053cc
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-18 15:23:33 +0530

    Added all the inital file

---
+++ app/src/main/java/com/apogee/registration/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.apogee.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.apogee.registration.databinding.ActivityMainBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
