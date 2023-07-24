25bbf0376e0793f5c48c16f4e03f72dc6e6a91b0
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-24 13:44:39 +0530

    Android Bluetooth status:Successfully

--- app/src/main/java/com/apogee/registration/ui/deivcelist/BluetoothDeviceListFragment.kt
+++ app/src/main/java/com/apogee/registration/ui/deivcelist/BluetoothDeviceListFragment.kt
@@ -3,12 +3,16 @@ package com.apogee.registration.ui.deivcelist
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.apogee.registration.R
 import com.apogee.registration.databinding.BluethoothDeviceListLayoutBinding
 import com.apogee.registration.datastore.RegistrationAppSharedPref
+import com.apogee.registration.utils.DataResponse
 import com.apogee.registration.utils.OnItemClickListener
+import com.apogee.registration.utils.createLog
 import com.apogee.registration.utils.displayActionBar
+import com.apogee.registration.viewmodel.BleConnectionViewModel
 import kotlinx.coroutines.launch
 
 class BluetoothDeviceListFragment : Fragment(R.layout.bluethooth_device_list_layout) {
@@ -17,6 +21,9 @@ class BluetoothDeviceListFragment : Fragment(R.layout.bluethooth_device_list_lay
     private val regSharedPref by lazy {
         RegistrationAppSharedPref.getInstance(requireContext())
     }
+
+    private val viewModel: BleConnectionViewModel by viewModels()
+
 
     private val mnuCallBack = object :
         OnItemClickListener {
@@ -40,6 +47,24 @@ class BluetoothDeviceListFragment : Fragment(R.layout.bluethooth_device_list_lay
             R.menu.info_mnu,
             onMenuItem = mnuCallBack
         )
+        getBleDevice()
+    }
 
+    private fun getBleDevice() {
+        viewModel.bleDeviceAvailable.observe(viewLifecycleOwner){
+            when(it){
+                is DataResponse.Error -> {
+                    createLog(
+                        "BLE_RES", "Error ${it.data} and Exp ${it.exception?.localizedMessage}"
+                    )
+                }
+                is DataResponse.Loading ->{
+                    createLog("BLE_RES", " LOADING ${it.data} ")
+                }
+                is DataResponse.Success -> {
+                    createLog("BLE_RES", " Success ${it.data} ")
+                }
+            }
+        }
     }
 }
