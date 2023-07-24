7feba9237b466b46ccfd8d93924ead33257ca885
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-24 13:00:00 +0530

    Added the Permission code status:Successfully

--- app/src/main/java/com/apogee/registration/ui/DashBoardActivity.kt
+++ app/src/main/java/com/apogee/registration/ui/DashBoardActivity.kt
@@ -1,8 +1,14 @@
 package com.apogee.registration.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.apogee.registration.databinding.DashboardActivtyLayoutBinding
+import com.apogee.registration.utils.PermissionUtils
+import com.permissionx.guolindev.PermissionX
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class DashBoardActivity : AppCompatActivity() {
     private lateinit var binding: DashboardActivtyLayoutBinding
@@ -11,6 +17,7 @@ class DashBoardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DashboardActivtyLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 
 }
--- app/src/main/java/com/apogee/registration/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/apogee/registration/ui/SplashScreenActivity.kt
@@ -2,12 +2,15 @@ package com.apogee.registration.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.apogee.registration.databinding.ActivityMainBinding
 import com.apogee.registration.datastore.RegistrationAppSharedPref
+import com.apogee.registration.utils.PermissionUtils
 import com.apogee.registration.utils.createLog
 import com.apogee.registration.utils.goToNextActivity
+import com.permissionx.guolindev.PermissionX
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -22,14 +25,25 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
         lifecycleScope.launch {
             delay(2000)
-            RegistrationAppSharedPref.getInstance(this@SplashScreenActivity).also {
-                createLog("TAG_LOGIN", "${it.getLoginResponse()}")
-                if (it.getLoginResponse() == null || it.getLoginResponse()!!.data.isNullOrEmpty()) {
-                    goToNextActivity<LoginActivity>(true)
-                } else {
-                    goToNextActivity<DashBoardActivity>(true)
+            PermissionX.init(this@SplashScreenActivity).permissions(PermissionUtils.permissions)
+                .request { allGranted, grantedList, deniedList ->
+                    if (allGranted) {
+                        RegistrationAppSharedPref.getInstance(this@SplashScreenActivity).also {
+                            createLog("TAG_LOGIN", "${it.getLoginResponse()}")
+                            if (it.getLoginResponse() == null || it.getLoginResponse()!!.data.isNullOrEmpty()) {
+                                goToNextActivity<LoginActivity>(true)
+                            } else {
+                                goToNextActivity<DashBoardActivity>(true)
+                            }
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@SplashScreenActivity,
+                            "These permissions are denied: $deniedList",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
-            }
         }
 
     }
