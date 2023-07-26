88cd392e0a415b55e3c75ad762869f06d105dcf7
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-26 15:28:25 +0530

    Connected the DeviceRegistration status:Successfully

--- app/src/main/java/com/apogee/registration/ui/registion/DeviceRegistrationFragment.kt
+++ app/src/main/java/com/apogee/registration/ui/registion/DeviceRegistrationFragment.kt
@@ -1,29 +1,36 @@
 package com.apogee.registration.ui.registion
 
+import android.bluetooth.BluetoothAdapter
 import android.content.ComponentName
+import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
 import android.view.View
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.apogee.blemodule.CommunicationLibrary.SerialListener
+import com.apogee.blemodule.CommunicationLibrary.SerialService
+import com.apogee.blemodule.CommunicationLibrary.SerialSocket
 import com.apogee.registration.R
 import com.apogee.registration.databinding.DeviceRegistrationLayoutBinding
+import com.apogee.registration.instance.BluetoothCommunication
 import com.apogee.registration.utils.calenderPicker
+import com.apogee.registration.utils.createLog
 import com.apogee.registration.utils.displayActionBar
 import com.apogee.registration.utils.showToastMsg
 
 class DeviceRegistrationFragment : Fragment(R.layout.device_registration_layout),ServiceConnection,SerialListener {
-    private lateinit var binding: DeviceRegistrationLayoutBinding
 
+    private lateinit var binding: DeviceRegistrationLayoutBinding
 
     private val args: DeviceRegistrationFragmentArgs by navArgs()
 
-
     private var isSubEdClicked = false
 
+    private var service: SerialService? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,11 +41,29 @@ class DeviceRegistrationFragment : Fragment(R.layout.device_registration_layout)
             bckIc = R.drawable.ic_back_arrow
         )
         setupUI()
-        /* activity?.bindService(Intent(requireActivity(), SerialService::class.java), this,
-             AppCompatActivity.BIND_AUTO_CREATE
-         )*/
+        activity?.bindService(
+            Intent(requireActivity(), SerialService::class.java), this,
+            AppCompatActivity.BIND_AUTO_CREATE
+        )
         binding.submitBtn.setOnClickListener {
             //showToastMsg(binding.modelNo.text.toString())
+            try {
+                val bluetoothAdapter = BluetoothCommunication(requireContext()).getBluetoothAdaptor()
+                val device = bluetoothAdapter.getRemoteDevice(args.macaddress)
+               // connected = Connected.Pending
+                val socket = SerialSocket(activity?.applicationContext, device)
+                service!!.connect(socket)
+            } catch (e: java.lang.Exception) {
+                onSerialConnectError(e)
+            }
+         /*   try {
+                val bleAdaptor=BluetoothAdapter.getDefaultAdapter()//BluetoothCommunication(requireContext()).getBluetoothAdaptor()
+                val device=bleAdaptor.getRemoteDevice(args.macaddress)
+                val service=SerialSocket(requireContext(),device)
+                this.service?.connect(service)
+            }catch (e:Exception){
+                this.service?.onSerialConnectError(e)
+            }*/
         }
     }
 
@@ -75,31 +100,42 @@ class DeviceRegistrationFragment : Fragment(R.layout.device_registration_layout)
 
     override fun onStart() {
         super.onStart()
-
+        service?.attach(this) ?: activity?.startService(
+            Intent(
+                requireActivity(),
+                SerialService::class.java
+            )
+        )
     }
 
-    override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
-        TODO("Not yet implemented")
+    override fun onServiceConnected(p0: ComponentName?, binder: IBinder?) {
+        showToastMsg("connection with service")
+        service = (binder as SerialService.SerialBinder).service
+        service?.attach(this)
     }
 
     override fun onServiceDisconnected(p0: ComponentName?) {
-        TODO("Not yet implemented")
+        showToastMsg("Disconnected")
     }
 
     override fun onSerialConnect() {
-        TODO("Not yet implemented")
+        showToastMsg("Connected")
     }
 
     override fun onSerialConnectError(p0: Exception?) {
-        TODO("Not yet implemented")
+        showToastMsg("Error While Connection")
+        createLog("TAG_ble"," connection Error ${p0?.localizedMessage}")
     }
 
     override fun onSerialRead(p0: ByteArray?) {
-        TODO("Not yet implemented")
+      p0?.let {
+          val string= String(p0)
+          createLog("TAG_ble","Response -> $string")
+      }
     }
 
     override fun onSerialIoError(p0: Exception?) {
-        TODO("Not yet implemented")
+        createLog("TAG_ble","Error -> ${p0?.localizedMessage}")
     }
 
     private fun setAdaptor(strAdp: Int): ArrayAdapter<String> {
