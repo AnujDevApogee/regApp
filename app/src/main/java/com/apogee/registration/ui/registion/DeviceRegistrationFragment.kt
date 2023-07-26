212740d8174cf1dcefc222ec77cd28b81e09b2be
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-26 13:35:09 +0530

    Confirm the UI status:Successfully

--- app/src/main/java/com/apogee/registration/ui/registion/DeviceRegistrationFragment.kt
+++ app/src/main/java/com/apogee/registration/ui/registion/DeviceRegistrationFragment.kt
@@ -1,15 +1,13 @@
 package com.apogee.registration.ui.registion
 
 import android.content.ComponentName
-import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.apogee.blemodule.CommunicationLibrary.SerialListener
-import com.apogee.blemodule.CommunicationLibrary.SerialService
 import com.apogee.registration.R
 import com.apogee.registration.databinding.DeviceRegistrationLayoutBinding
 import com.apogee.registration.utils.displayActionBar
@@ -17,6 +15,8 @@ import com.apogee.registration.utils.displayActionBar
 class DeviceRegistrationFragment : Fragment(R.layout.device_registration_layout),ServiceConnection,SerialListener {
     private lateinit var binding: DeviceRegistrationLayoutBinding
 
+
+    private val args: DeviceRegistrationFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,16 +26,22 @@ class DeviceRegistrationFragment : Fragment(R.layout.device_registration_layout)
             binding.actionLayout,
             bckIc = R.drawable.ic_back_arrow
         )
-        activity?.bindService(Intent(requireActivity(), SerialService::class.java), this,
-            AppCompatActivity.BIND_AUTO_CREATE
-        )
+        setupUI()
+        /* activity?.bindService(Intent(requireActivity(), SerialService::class.java), this,
+             AppCompatActivity.BIND_AUTO_CREATE
+         )*/
 
+    }
+
+    private fun setupUI() {
+        binding.bleNme.setText(args.devicename)
     }
 
     override fun onStart() {
         super.onStart()
 
     }
+
     override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
         TODO("Not yet implemented")
     }
